307a78cf7358d6b4b39fbf471b513ef3ee0208fb
Bilal Kanjelkheir <bilal.kanjelkheir@gmail.com>
2025-08-21 23:39:26 +0300

    Create devices database schema (up migration and downmigration)

---
+++ backend/sql/schema/001_devices.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+create table devices (
+    id varchar(36) primary key,
+    model text unique not null,
+    twrp text unique,
+    stock_firmware text unique,
+    odin text unique,
+    orange_fox_recovery text,
+    custom_firmeware text,
+    shrp_recovery text unique
+);
